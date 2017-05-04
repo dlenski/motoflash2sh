b56e1563efa80436de9f132fc023fbbadf1f2bba
Daniel Lenski <dlenski@gmail.com>
2017-05-03 21:47:46 -0700

    filling in bits

--- motoflash2sh.py
+++ motoflash2sh.py
@@ -9,7 +9,7 @@ def commentify(x, prefix='# '):
     return indent(ElementTree.tostring(x).strip().decode(), prefix)
 
 def parse_args(args=None):
-    p = argparse.ArgumentParser()
+    p = argparse.ArgumentParser(description="Converts Motorola's flashfile.xml format to a [ba]sh script of fastboot commands")
     p.add_argument('FLASHFILE_XML')
     p.add_argument('-o', '--output', metavar='FLASHFILE_SH', type=argparse.FileType('w'),
                    help='Script to output (default is flashfile.sh in same directory)')
@@ -26,7 +26,10 @@ def parse_args(args=None):
 def main():
     p, args = parse_args()
 
-    x = ElementTree.parse(args.FLASHFILE_XML)
+    try:
+        x = ElementTree.parse(args.FLASHFILE_XML)
+    except ElementTree.ParseError as e:
+        p.error('XML parse error: %s' % e.args)
     if x.getroot().tag != 'flashing':
         p.error('Input does not appear to be a Motorola flashfile.xml (root node is not <flashing>)')
 
@@ -43,12 +46,13 @@ def main():
     steps = x.find('steps')
     if not steps:
         args.output.write('# WARNING: no <steps> found\n')
-        return
+        steps = ()
 
     if args.verify:
         md5sums = odict()
         sha1sums = odict()
-        for step in steps.iter('step'):
+        for step in steps.getchildren():
+            assert step.tag == 'step'
             if 'MD5' in step.keys() and 'filename' in step.keys():
                 md5sums[step.get('filename')] = step.get('MD5')
             elif 'SHA1' in step.keys() and 'filename' in step.keys():
@@ -59,7 +63,8 @@ def main():
         if sha1sums:
             args.output.write('sha1sum --check <<EOF || exit 1\n' + ''.join('{1} *{0}\n'.format(*p) for p in sha1sums.items()) + 'EOF\n\n')
 
-    for nn, step in enumerate(steps.iter('step')):
+    for nn, step in enumerate(steps.getchildren()):
+        assert step.tag == 'step'
         op = step.get('operation')
         if op=='flash':
             vars = 'partition', 'filename',
@@ -68,13 +73,19 @@ def main():
         elif op in ('getvar','oem'):
             vars = 'operation',
         else:
-            args.output.write('\n# %02d: unknown operation %r:\n%s\n\n' % (nn, op, commentify(step)))
+            args.output.write('\n# %02d: skipping unknown operation %r:\n%s\n\n' % (nn, op, commentify(step)))
             op = None
 
         if op:
             if args.verbose:
                 args.output.write(commentify(step, '# %02d: ' % nn) + '\n')
             args.output.write('fastboot %s || exit 1\n' % ' '.join(step.get(v) for v in vars))
+
+    print("Wrote shell script with %d fastboot steps to %s" % (len(steps), args.output.name),
+          file=sys.stderr)
+
+    print("Wrote shell script with %d fastboot steps to %s" % (len(steps), args.output.name),
+          file=sys.stderr)
 
 if __name__=='__main__':
     sys.exit( main() )
